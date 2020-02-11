55a1df898b3f3b9edca9f99c0e072544046c4a31
John James Ermitano <johnjamesermitano@yahoo.com>
2020-02-11 21:14:04 +0800

    chore: to poly typescript

--- server/dist/app.js
+++ server/dist/app.js
@@ -7,21 +7,16 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const app = express_1.default();
-const port = process.env.PORT || 3001;
 dotenv_1.default.config();
+const app = express_1.default();
+const port = +process.env.PORT || 8082;
 app.use(cors_1.default());
 app.use(express_1.default.json());
 const uri = process.env.ATLAS_URI;
 mongoose_1.default.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose_1.default.connection;
 connection.once('open', () => {
-    console.log("\nMongoDB database connection established successfully");
+    console.log("\nMongoDB database connection established successfully\n");
 });
-app.get('/', (req, res) => () => {
-    console.log('HW');
-});
-app.listen(port, () => {
-    console.log(`\nListening on port ${port}!`);
-});
+app.listen(port, () => console.log(`\nListening on port ${port}!`));
 //# sourceMappingURL=app.js.map
